6723db01c8516a90af528418b753b2b4e88f4c11
Jose <josefbautista94@gmail.com>
2025-03-13 15:40:29 -0400

    Completed part 4

--- index.js
+++ index.js
@@ -2,21 +2,27 @@
 
 // Menu data structure
 let menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-      {text: 'all', href: '/catalog/all'},
-      {text: 'top selling', href: '/catalog/top'},
-      {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-      {text: 'new', href: '/orders/new'},
-      {text: 'pending', href: '/orders/pending'},
-      {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-      {text: 'profile', href: '/account/profile'},
-      {text: 'sign out', href: '/account/signout'},
-    ]},
+    { text: 'about', href: '/about' },
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
 ];
 
 let mainEl = document.querySelector("main"); //caching the main element into a variable named mainEl
@@ -34,7 +40,7 @@ subMenuEl.style.height = "100%"
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
 subMenuEl.classList.add("flex-around")
 subMenuEl.style.position = "absolute"
-subMenuEl.style.top ="0" // hides the subMenu
+subMenuEl.style.top = "0" // hides the subMenu
 
 // for (let i = 0; i < menuLinks.length; i++) { //looping through menusLink
 //   console.log(menuLinks[i]);
@@ -45,17 +51,32 @@ subMenuEl.style.top ="0" // hides the subMenu
 // }
 
 menuLinks.forEach((stuff) => {
-  //decided to do it for a for each instead
-  let a = document.createElement("a"); // creating the anchor tags
-  a.setAttribute("href", stuff.href); // retriving the href from menuLinks array
-  a.textContent = stuff.text; //retriving text value from menu array
-  topMenuEl.appendChild(a); //adding the anchor elements to topMenuEl
+    //decided to do it for a for each instead
+    let a = document.createElement("a"); // creating the anchor tags
+    a.setAttribute("href", stuff.href); // retriving the href from menuLinks array
+    a.textContent = stuff.text; //retriving text value from menu array
+    topMenuEl.appendChild(a); //adding the anchor elements to topMenuEl
 });
 
 
 //Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
-let topMenuLinks =  document.querySelectorAll("#top-menu a") // selects all ellements that match a css selector unlike query selector that only selects the first match
+let topMenuLinks = document.querySelectorAll("#top-menu a") // selects all ellements that match a css selector unlike query selector that only selects the first match
 // querySelectorAll returns a NodeList aka a collection of Elements
 //#top-menu finds the element with the id = "top-menu"
 //a selects all <a> elements links inside of #top-menu 
 
+// Attach a delegated 'click' event listener to topMenuEl.
+topMenuEl.addEventListener("click", function (event) { // listening for any clicks in the topMenuEl, whem any element inside of the topMenuEl is clicked the functon runs
+    event.preventDefault();  // Prevents default link behavior, we do this so you can handle the menu interaction yourself (highlighting, showing submenus, etc.), The browser does not navigate away
+
+    //The second line of code of the function should immediately return if the element clicked was not an <a> element.
+    if (!event.target.matches('a')) { //event.target represents the exact element that was clicked, .matches(a) checks if the clicked element was an anchor tag <a>
+        return; //This check makes sure the event only runs for actual menu links
+    }
+    console.log(event.target.textContent) // it works!
+
+    topMenuLinks.forEach(link => link.classList.remove("active")); // Before adding "active" to the clicked link, remove it from all links to prevent multiple highlights!
+
+    event.target.classList.add("active"); // Adds "active" class to what ever link is being clicked on
+
+})
