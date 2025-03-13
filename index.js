6e5db8c1e08e1333ff4eda1e3e252c7aba744683
Jose <josefbautista94@gmail.com>
2025-03-13 13:37:24 -0400

    updated the menuLinks array

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 
 // Menu data structure
 let menuLinks = [
-  // menu links that we're going to implement in a for loop later to put on the navbar
-
-  { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
 ];
 
 let mainEl = document.querySelector("main"); //caching the main element into a variable named mainEl
